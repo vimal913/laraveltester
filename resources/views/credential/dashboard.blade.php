4cdf60e0b4f463dd45e23045241d484547c14a8a
VIMAL S <CIPL1208@colanonline.local>
2023-01-05 18:11:42 +0530

    dashboard-blade

---
+++ resources/views/credential/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('credential.main')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">Dashboard</div>
+    <div class="card-body">
+        Your are logged in....:)
+    </div>
+</div>
+    
+@endsection
